55bbf1df4ce436c1599fbdfe6a1fab6b9ddd9e55
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-02-27 21:51:16 +0100

    updating friedns by identification find one and update

--- controllers/friendController.js
+++ controllers/friendController.js
@@ -1,5 +1,19 @@
 const { User } = require('../models');
-//Error Response
+//faillure feedback 
 const catchError = (res, error) => {
     res.status(500).json({ error });
 };
+
+//updating friends by identification
+const updateFriends = async (req, res, update) => {
+    try {
+        const userDb = await User.findOneAndUpdate(
+            { _id: req.params.userId },
+            update,
+            { new: true }
+        );
+        res.status(200).json(userDb);
+    } catch (error) {
+        catchError(res, error);
+    }
+}

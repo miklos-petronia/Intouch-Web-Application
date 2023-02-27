4ca726f172ebe7405c01f6b8e84beb1b6ed177be
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-02-27 21:59:31 +0100

    developing a new feedback/response on a thought

--- controllers/reactionController.js
+++ controllers/reactionController.js
@@ -3,3 +3,16 @@ const { Thought } = require('../models');
 const catchError = (res, error) => {
     res.status(500).json({ error });
 };
+//Developing a new feedback on an allied thought
+const createReaction = async (req, res) => {
+    try {
+        const updatedThought = await Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $addToSet: { reactions: req.body } },
+            { runValidators: true, new: true }
+        );
+        res.status(200).json(updatedThought);
+    } catch (error) {
+        catchError(res, error);
+    }
+};

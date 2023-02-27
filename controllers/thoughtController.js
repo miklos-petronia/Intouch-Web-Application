365b0ff68886bb43e68d3d0753a1f916d49ed0f1
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-02-27 22:05:06 +0100

    developing a thought established on the application body premise

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -4,3 +4,17 @@ const { Thought, User } = require('../models');
 const catchError = (res, error) => {
     res.status(404).json({ msg: `An Error occurred`, error });
 };
+//developing a thought and allied it with a user established on the application body params.
+const createThought = async (req, res) => {
+    try {
+        const thought = await Thought.create(req.body);
+        const userDb = await User.findOneAndUpdate(
+            { username: req.body.username },
+            { $addToSet: { thoughts: thought._id } },
+            { new: true }
+        );
+        res.status(200).json({ thought, userDb });
+    } catch (error) {
+        catchError(res, error);
+    }
+};

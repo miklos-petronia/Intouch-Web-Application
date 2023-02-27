2a2e6736ff85ea6b80aba491952e4da980e5b767
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-02-27 21:42:25 +0100

    added a connection string

--- config/connection.js
+++ config/connection.js
@@ -2,3 +2,10 @@ const { connect, connection } = require('mongoose');
 
 const connectionString =
     process.env.MONGODB_URI || 'mongodb://localhost:27017/socialbackend';
+
+connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+module.exports = connection;

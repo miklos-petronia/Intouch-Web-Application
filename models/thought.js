1768c8ed403290ee896f76f8eb9ad5eed4687846
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-02-28 21:27:19 +0100

    created a schema for thoughts

--- models/thought.js
+++ models/thought.js
@@ -25,3 +25,35 @@ const ReactionSchema = new Schema(
         id: false,
     }
 );
+
+const ThoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => {
+            return new Intl.DateTimeFormat('en-US').format(timestamp);
+        },
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [ReactionSchema]
+});
+
+ThoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+ThoughtSchema.set('toJSON', {
+    getters: true
+});
+
+const Thought = mongoose.model('Thought', ThoughtSchema);
+
+module.exports = Thought;

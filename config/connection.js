d21c2a15ad38b560a1eb6ca6a5c6a1419c101e95
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-03-05 20:54:23 +0100

    update

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const { connect, connection } = require('mongoose');
 
 const connectionString =
-    process.env.MONGODB_URI || 'mongodb://localhost:27017/socialbackend';
+    process.env.MONGODB_URI || 'mongodb://localhost:27017/intouchweb';
 
 connect(connectionString, {
     useNewUrlParser: true,
